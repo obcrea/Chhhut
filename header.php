d5da55f9455a23eff22116ec1411f9a1eec77369
obcrea <olivierbertolotti@MacBook-Pro-de-Olivier.local>
2014-09-15 18:33:32 +0200

    Day6
    suite

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title><?php echo $title ?></title>
+  		<meta name="description" content="<?php echo $meta_description; ?>">
+  		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	    <!-- Leave those next 4 lines if you care about users using IE8 -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+		<link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+		<link href="style.css" rel="stylesheet">
+	</head>
+		<header>
+			<div class="row">
+				<div class="col-md-offset-3 col-md-6 contact text-center">
+						<p>22, rue Béranger - Paris</p>
+						<p>Tél : 09 53 76 74 22 - xxx@Chhhut.com</p>
+				</div>		
+				<div class="col-md-3 contact text-center">
+					<ul>		
+						<li><a href:"#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+						<li><a href:"#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+						<li><a href:"#"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+					</ul>		
+				</div>
+			</div>	
+			<div class="row">
+			<nav class="navbar navbar-default" role="navigation">
+				  <div class="container-fluid">
+				  <div class="navbar-header">
+				      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+	    </div>
+				    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				      <ul class="nav navbar-nav">
+				        <li class="active border"><a href="#">Accueil</a></li>
+				        <li class="border"><a href="#">La Carte</a></li>
+				        <li class="border"><a href="#">Les Vins</a></li>
+				        <li class="border"><a href="#">News</a></li>
+				        <li class="border"><a href="#">Presse</a></li>
+				        <li><a href="#">Contact</a></li>
+				      </ul>  
+				    </div><!-- /.navbar-collapse -->
+				  </div><!-- /.container-fluid -->
+			</nav>		
+			</div>		    	
+	    </header>
